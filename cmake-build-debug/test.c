e3d8128c331f01879a4c20ff12860658d19a355b
abhiyanshakya <abhiyamshakya64@gmail.com>
2026-05-26 11:00:21 +0930

    add test.c

---
+++ cmake-build-debug/test.c
@@ -0,0 +1,23 @@
+int {
+    int a;
+    int b;
+    char c;
+    a = 5;
+    b = 0;
+    c = 30;
+
+    if a {
+        while b {
+            b = b + 1;
+        }
+        for i {
+            _counter = i;
+        }
+    } else {
+        do {
+            a = a + 1;
+        } while a;
+    }
+
+    return a;
+}
